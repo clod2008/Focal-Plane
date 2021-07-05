028e4c5418737c1e964ad78f3a1703e7a68e27e3
clod2008 <claudio@apsis.com.ar>
2021-07-05 15:08:15 -0300

    Focal Plane 0.1

---
+++ assets/js/data.js
@@ -0,0 +1,20 @@
+const data = {
+    phone:  ' +1 5555 55555 55',
+    email:  ' info@test-email.mail',
+    adress: ' A108 Adam Street, New York, NY 535022'
+}
+
+// const dataInput = () => {
+//     let phoneNum = document.querySelectorAll('phoneNum');
+//     console.log(phoneNum);
+//     for(i=0; i < phoneNum.length; i++){
+        
+//         phoneNum.append(data.phone)
+//     }
+    // phoneNum.append(data.phone);
+
+    // let emailTx = document.querySelectorAll('emailTx');
+    // emailTx.append(data.email);
+// }
+
+// dataInput()
